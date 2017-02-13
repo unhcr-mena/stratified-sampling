ba976c43530b76b7411366a19be89ca2d6bead7c
Edouard <edouard.legoupil@gmail.com>
2017-02-13 13:28:19 +0200

    Fixed typos

--- code/multivariate_strata.R
+++ code/multivariate_strata.R
@@ -1,10 +1,12 @@
-## This script allow to generate stratified samples usung proportional allocation
+
+
+## This script allows to generate stratified samples using proportional allocation
 
 
 
-#########################################################
+#############################################################
 ## STEP 1: Install & load Required Library
-#########################################################
+#############################################################
 
 ### uncomment the next tow lines at first utilisation
 #install.packages("simFrame")
@@ -14,9 +16,9 @@ library(simFrame)
 library(sampling)
 
 
-#########################################################
+#############################################################
 ## STEP 2: Generate the variables and the dataset
-#########################################################
+#############################################################
 # PS: note that you can skip that step if you have already your dataset
 
 #Confirm the the population size called here N
@@ -34,23 +36,24 @@ needs <- sample(x=c(0,1), size=N, replace=TRUE, prob=c(.45,.55))
 data <- data.frame(size, return, sex, region, needs)
 rm(size, return, sex, region, needs)
 
-#########################################################
+#############################################################
 ## STEP 3: Insert your configuration for the sample
-#########################################################
+#############################################################
 
 
 
-#  Decide  the confidence level
+#  Decide on the confidence level
 #  It represents the probability of the same result if you re-sampled, all other things equal.
 # A measure of how certain you are that your sample accurately reflects the population, within its margin of error.
 ## Common standards used by researchers are 90%, 95%, and 99%.
 z <- 1.95
 
-# Decide on the margin of error - Precision is the variability of the estimate. 
-# A percentage that describes how closely the answer your sample gave is to the “true value” is in your population. 
+# Decide on the margin of error - Precision  
+# This percentage describes the variability of the estimate: how closely the answer your sample gave is
+# to the “true value” is in your population. 
 # The smaller the margin of error is, the closer you are to having the exact answer at a given confidence level.
 # A smaller margin of error means that you must have a larger sample size given the same population.
-## A common value is: ± 5%)
+## Common standards used by researchers are: ± 5%, ± 3% , ± 1%)
 e <- 0.05
 
 #fill the proportion of the attribute
@@ -64,9 +67,9 @@ e <- 0.05
 p <- 0.5
 q <- 1-p
 
-#########################################################
+#############################################################
 ## STEP 4: Calculate the sample size
-#########################################################
+#############################################################
 #
 
 #compute the sample size for a large population
@@ -80,11 +83,11 @@ n <- n0/(1+((n0-1)/N))
 n <- round(n, digits = 0)
 
 
-#########################################################
-## STEP 5: stratify the dataset
-#########################################################
+#############################################################
+## STEP 5: Stratify the dataset using proportional allocation
+#############################################################
 
-# We buidl the 'Strata' object
+# We build the 'Strata' object
 st <- stratify(data, c("size", "needs"))
 #summary(st)
 str(st)
@@ -104,6 +107,7 @@ stratified_sample <- strata(data, c("size", "needs"), c(n_size), method=("srswor
 summary(stratified_sample)
 data_sampled <- getdata(data, stratified_sample)
 #print(data_sampled)
+write.csv(data_sampled, "data_sampled.csv")
 
 #check if the the sample is good by checkin if the proportion of the attribute in the sample
 # is close to its population's counterpart
